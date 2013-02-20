3af3c0aa9828cf3aadde50aea0647a3e1eaab068
Jurica Shell <seva.jurica@gmail.com>
2013-02-20 19:40:38 +0100

    length

--- python/H1/l.py
+++ python/H1/l.py
@@ -1,4 +1,4 @@
-import MySQLdb
+import MySQLdb, sys
 
 def dbQuery(sql):
 
@@ -116,7 +116,7 @@ def runParallelCategory():
     
     for index in inputs:
         #print index
-        jobs.append(job_server.submit(createData, (index,), depfuncs = (dbQuery, getMainCat, ), modules = ("MySQLdb",)))    
+        jobs.append(job_server.submit(createData, (index,), depfuncs = (dbQuery, getMainCat, ), modules = ("MySQLdb","sys",)))    
     for job in jobs:
         result = job()
         if result:
