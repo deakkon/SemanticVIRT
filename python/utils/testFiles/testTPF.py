5b7730436a470c3d6db7bc2603801ce3d53216d4
Jurica Shell <seva.jurica@gmail.com>
2013-04-26 23:06:56 +0200

    full example. not working. ShevaTPF -> testFiles/testTPF

--- python/utils/testFiles/testTPF.py
+++ python/utils/testFiles/testTPF.py
@@ -4,7 +4,7 @@ path.append("/home/jseva/SemanticVIRT/python/utils/")
 from ShevaTPF import ShevaTPF
 
 text = "i was ALWAYS dreaming about the sunsets over the hill in the blah. maria.<html><h1>"
-tfp = ShevaTPF("a","b")
+tfp = ShevaTPF(text,1)
 tfpResult = tfp.returnClean()
 
 print text,"\n"
